37b28226dcd996765dba297b1f4bd71c874732c4
Moneem1 <125930635+Moneem1@users.noreply.github.com>
2024-03-09 16:21:04 +0100

    BookBrokerApplication class  that has main method to running Book broker app

---
+++ src/main/java/com/book_broker/moneem/BookBrokerApplication.java
@@ -0,0 +1,13 @@
+package com.book_broker.moneem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookBrokerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookBrokerApplication.class, args);
+	}
+
+}
